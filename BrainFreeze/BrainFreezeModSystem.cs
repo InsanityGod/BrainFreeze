d4df761d9617ad963848cc0bc55aabc5b90e2372
_Kristof_ <krizziewissie@gmail.com>
2024-12-29 15:06:12 +0100

    Progress

--- BrainFreeze/BrainFreezeModSystem.cs
+++ BrainFreeze/BrainFreezeModSystem.cs
@@ -65,14 +65,17 @@ namespace BrainFreeze
         }
 
         public override void AssetsFinalize(ICoreAPI api)
-                {
+        {
             if(api.Side == EnumAppSide.Client) return;
+
+            waterBlockId = api.World.GetBlock(AssetLocation.Create("water-still-7")).BlockId;
+            iceBlockId = api.World.GetBlock(AssetLocation.Create("lakeice")).BlockId;
             foreach(var item in api.World.Items)
             {
                 if (item.Variant["brainfreeze"] != null)
                 {
                     DynamicFrozenVariant.FinalizeFrozenCollectible(api, item);
-        }
+                }
             }
             base.AssetsFinalize(api);
         }
@@ -83,10 +86,91 @@ namespace BrainFreeze
             base.Dispose();
         }
 
-        public override void AssetsFinalize(ICoreAPI api)
-        {
+        internal int waterBlockId = -1;
+        internal int iceBlockId = -1;
 
-            //TODO
+
+        internal ICoreServerAPI serverAPI;
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            serverAPI = api;
+            if(!GlobalConstants.MeltingFreezingEnabled) return;
+            api.Event.RegisterGameTickListener(DoWaterStuff, 1000);
+            api.Event.ChunkDirty += OnChunkDirty;
+        }
+
+        private void DoWaterStuff(float lastCalled)
+        {
+            var block = serverAPI.World.GetBlockAccessorBulkUpdate(true, false);
+            
+            //serverAPI.WorldManager.Load
+            //TODO performance
+            var loadedChunks = serverAPI.WorldManager.AllLoadedChunks;
+
+            bool anyFreezing = false;
+            foreach((var key, var chunk) in loadedChunks)
+            {
+                if(chunk.LiveModData.TryGetValue("BrainFreeze:WaterMap", out object value) && value is WaterMap waterMap && waterMap.ContainsWater)
+                {
+                    waterMap.DoFreezing(this, block, chunk);
+                    anyFreezing = true;
+                }
+            }
+
+            if (anyFreezing)
+            {
+                block.Commit();
+            }
+        }
+
+        private void OnChunkDirty(Vec3i chunkCoord, IWorldChunk chunk, EnumChunkDirtyReason reason)
+        {
+            if(chunk.Data == null) return;
+            //TODO optimize
+            var waterMap = (WaterMap) (
+                chunk.LiveModData.TryGetValue("BrainFreeze:WaterMap", out object value) ? 
+                value :
+                chunk.LiveModData["BrainFreeze:WaterMap"] = new WaterMap()); //32 ^ 2
+
+            for (int z = 0; z < MagicNum.ServerChunkSize; z++)
+            {
+                int zOffset = z * MagicNum.ServerChunkSize; // Precompute the Z contribution
+
+                for (int x = 0; x < MagicNum.ServerChunkSize; x++)
+                {
+                    int xzOffset = zOffset + x; // Precompute the ZX contribution
+
+                    bool found = false;
+                    for (ushort y = chunk.MapChunk.YMax; y > 0; y--)
+                    {
+                        int index3D = xzOffset * MagicNum.ServerChunkSize + y; // Add Y contribution
+
+                        var fluidId = chunk.Data.GetFluid(index3D);
+                        if (fluidId == waterBlockId || fluidId == iceBlockId)
+                        {
+                            waterMap[xzOffset] = y;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        waterMap[xzOffset] = 0;
+                    }
+                }
+            }
+
+            if(reason == EnumChunkDirtyReason.NewlyLoaded || reason == EnumChunkDirtyReason.NewlyCreated)
+            {
+                //TODO see about freezing afterwards to a realistic point
+            }
+        }
+
+        private void OnChunkLoaded(Vec2i chunkCoord, IWorldChunk[] chunks)
+        {
+            throw new NotImplementedException();
         }
 
 
---
+++ BrainFreeze/WaterMap.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.Server;
+
+namespace BrainFreeze
+{
+    public class WaterMap
+    {
+        private ushort[] waterMap;
+
+        private int waterCount = 0;
+        
+        public bool ContainsWater => waterCount > 0;
+
+        public ushort this[int index]
+        {
+            get
+            {
+                if(waterMap == null) return 0;
+                return waterMap[index];
+            }
+            set
+            {
+
+                waterMap ??= new ushort[MagicNum.ServerChunkSize * MagicNum.ServerChunkSize];
+                
+                if (waterMap[index] != value)
+                {
+                    waterMap[index] = value;
+                    if(value == 0)
+                    {
+                        waterCount--;
+                    }
+                    else
+                    {
+                        waterCount++;
+                    }
+                }
+            }
+        }
+
+        public void DoFreezing(BrainFreezeModSystem system, IBulkBlockAccessor block, IServerChunk chunk)
+        {
+            var totalDays = system.serverAPI.World.Calendar.TotalDays;
+            //TODO use totalDays and chunk to see how much time has passed
+            //TODO further refine this logic
+            for(int i = 0; i < waterMap.Length; i++) 
+            {
+                if (waterMap[i] == 0) continue;
+
+                double chance = 0d;
+
+                if(system.serverAPI.World.Rand.NextDouble() < chance)
+                {
+                    //system.serverAPI.World
+                    //var temperature = block.GetClimateAt(chunk.MapChunk.)
+                }
+            }
+        }
+    }
+}
