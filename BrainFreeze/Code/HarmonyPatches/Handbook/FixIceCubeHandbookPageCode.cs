a3470ff1250a2592eecc89808d4fb4b824bcd20d
_Kristof_ <krizziewissie@gmail.com>
2025-01-04 15:32:03 +0100

    fix handbook stuff

---
+++ BrainFreeze/Code/HarmonyPatches/Handbook/FixIceCubeHandbookPageCode.cs
@@ -0,0 +1,35 @@
+﻿using BrainFreeze.Code.Items;
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace BrainFreeze.Code.HarmonyPatches.Handbook
+{
+    [HarmonyPatch(typeof(GuiHandbookItemStackPage))]
+    public static class FixIceCubeHandbookPageCode
+    {
+        [HarmonyPatch(nameof(GuiHandbookItemStackPage.PageCodeForStack))]
+        [HarmonyPrefix]
+        public static bool PageCodeForStackPrefix(ItemStack stack, ref string __result)
+        {
+            if(stack.Collectible is IceCube iceCube)
+            {
+                var content = iceCube.GetContent(stack);
+
+                if(content != null)
+                {
+                    __result =  $"{stack.Class.Name()}-{iceCube.Code.ToShortString()}-{content.Id}";
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
