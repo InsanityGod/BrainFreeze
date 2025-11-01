d252bd00d322b92b5175d135b9cd5a3601c9fbba
_Kristof_ <krizziewissie@gmail.com>
2025-11-01 14:03:37 +0100

    cleanup unnused using statements

--- BrainFreeze/Code/HarmonyPatches/HydrateOrDieDrate/AssetsFinalizePatch.cs
+++ BrainFreeze/Code/HarmonyPatches/HydrateOrDieDrate/AssetsFinalizePatch.cs
@@ -1,10 +1,5 @@
 ﻿using HarmonyLib;
 using HydrateOrDiedrate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vintagestory.API.Common;
 
 namespace BrainFreeze.Code.HarmonyPatches.HydrateOrDieDrate;
