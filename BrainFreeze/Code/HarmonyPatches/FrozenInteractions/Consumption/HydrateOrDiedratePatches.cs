b8a81471ec35990d7d5b9386d32941e37319f5d4
_Kristof_ <krizziewissie@gmail.com>
2026-04-29 23:47:59 +0200

    add fallback for frozen liquid Hydration Calculation

--- BrainFreeze/Code/HarmonyPatches/FrozenInteractions/Consumption/HydrateOrDiedratePatches.cs
+++ BrainFreeze/Code/HarmonyPatches/FrozenInteractions/Consumption/HydrateOrDiedratePatches.cs
@@ -1,6 +1,9 @@
 ﻿using BrainFreeze.Code.Items;
 using HarmonyLib;
 using HydrateOrDiedrate;
+using InsanityLib.Extended.Transitions;
+using InsanityLib.Extensions;
+using System.Linq;
 using Vintagestory.API.Common;
 
 namespace BrainFreeze.Code.HarmonyPatches.FrozenInteractions.Consumption;
@@ -18,6 +21,29 @@ public static class HydrateOrDiedratePatches
             if(content != null)
             {
                 __result = HydrationManager.GetHydration(content);
+
+                if(__result <= 0)
+                {
+                    //TODO create extra helper methods for this in InsanityLib
+                    //Double check original liquid
+                    var meltTransition = content.Collectible.TransitionableProps.FirstOrDefault(static trans =>
+                    {
+                        var handler = CustomTransition.ExtendedEnum.FindHandler(trans.Type);
+
+                        return handler is not null && handler.TransitionCode.Domain == "brainfreeze" && handler.TransitionCode.Path == "melt";
+                    });
+
+                    if(meltTransition is not null)
+                    {
+                        var newStack = meltTransition.TransitionedStack.ResolvedItemStack?.Clone();
+                        if(newStack is not null)
+                        {
+                            newStack.StackSize = (int)(meltTransition.TransitionRatio * itemStack.StackSize);
+
+                            __result = HydrationManager.GetHydration(newStack);
+                        }
+                    }
+                }
                 return false;
             }
         }
